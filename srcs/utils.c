db86f8595a8e38651aed5242d1439f70f23010a9
ahocine <ahocine@student.42.fr>
2022-01-19 17:54:11 +0100

    A

--- srcs/utils.c
+++ srcs/utils.c
@@ -10,30 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "../push_swap.h"
 
-void	ft_swap(int *tab_to_swap)
+void	ft_swap(int *tab_a)
 {
 	// SWAP LES DEUX PREMIERES VALEURS
 	int	tmp;
 
-	if (!tab_to_swap[2])
-		return ;
-	tmp = tab_to_swap[0];
-	tab_to_swap[0] = tab_to_swap[1];
-	tab_to_swap[1] = tmp;
-}
-
-void	ft_push(int *tab_a, int *tab_b)
-{
-	int	tmp;
-
 	tmp = tab_a[0];
-	tab_a[0] = tab_b[0];
-	tab_b[0] = tmp;
-}
-
-void	ft_shift()
-{
-
+	tab_a[0] = tab_a[1];
+	tab_a[1] = tmp;
 }
